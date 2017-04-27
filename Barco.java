4b52085b831769431367b3fc516daaacb13f91ec
Silvia <silvia.quintanar.robles@gmail.com>
2017-04-27 21:25:10 +0200

    Terminadas clses Persona, Barco, Yate, Velero y EmbarcacionAMotor

---
+++ Barco.java
@@ -0,0 +1,62 @@
+//Source file: C:\\USERS\\ZIVY\\DESKTOP\\Programacion\\ProyectoAlquileres\\Barco.java
+
+
+public abstract class Barco 
+{
+   private String nMatricula;
+   private double eslora;
+   private int anioFabricacion;
+   private Persona duenioBarco;
+       
+   /**
+   @param matricula
+   @param eslora
+   @param duenio
+    */
+   public Barco(String nMatricula, double eslora, int anioFabricacion, Persona duenioBarco) 
+   {
+       this.nMatricula = nMatricula;
+       this.eslora = eslora;
+       this.anioFabricacion = anioFabricacion;
+       this.duenioBarco = duenioBarco;
+   }
+   
+   /**
+   */
+   public String getNMatricula() 
+   {
+       return nMatricula;
+   }
+   
+   /**
+    */
+   public double getEslora() 
+   {
+       return eslora;
+   }
+   
+   /**
+    */
+   public int getAnioFabricaion() 
+   {
+       return anioFabricacion;
+   }
+   
+     /**
+    */
+   public Persona getDuenioBarco() 
+   {
+       return duenioBarco;
+   }
+   
+
+   public abstract int getCoeficienteBernua() ;
+      
+   /**
+    */
+   public String toString() 
+   {
+       String textoAMostrar = duenioBarco.toString() + " Matrícula: " + nMatricula + "\n Eslora: " + eslora + "\n Año de fabricación: " + anioFabricacion;
+       return textoAMostrar;
+   }
+}
